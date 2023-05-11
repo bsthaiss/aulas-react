9afa4333e6a2fc5fd16df0d33e574dd8c99bad5d
Thais Barbosa <thaisbs1405@gmail.com>
2023-05-10 22:56:17 -0300

    Atualizei o código

--- calculadora-de-gorjeta/src/components/Calculadora.jsx
+++ calculadora-de-gorjeta/src/components/Calculadora.jsx
@@ -3,12 +3,42 @@ export function Calculadora() {
         <main>
             <form action="">
                 <div className="bill">
-                    <label htmlFor="">Conta</label>
+                    <label htmlFor="bill">Conta</label>
                 </div>
                 <div className="input-box">
-                    <input type="number" />
+                    <input type="number" id="bill" placeholder="0"/>
+                </div>
+                <div className="tip-percentage">
+                    <label htmlFor="custom">Selecione a Porcentagem %</label>
+                    <div>
+                        <input type="button" value="5%" /><input type="button" value="10%" /><input type="button" value="15%" /><input type="button" value="25%" /><input type="button" value="50%" />
+                        <input type="number" id="custom" placeholder="Outra" />
+                    </div>
+                </div>
+                <div className="people">
+                    <label htmlFor="people-number">Número de Pessoas</label>
+                    <div className="input-box">
+                        <input type="number" id="people-number" placeholder="0" />
+                    </div>
                 </div>
             </form>
+            <section className="results">
+                <div className="tip-amount">
+                    <p>
+                        Gorjeta <br />
+                        <span>/ pessoa</span>
+                    </p>
+                    <p>R$ 0.00</p>
+                </div>
+                <div className="total">
+                    <p>
+                        Total <br />
+                        <span>/ pessoa</span>
+                    </p>
+                    <p>R$ 0.00</p>
+                </div>
+                <button>Limpar</button>
+            </section>
         </main>
     );
 }
