fc3519c75db274f2c35bac64067ff335e0940859
Thais Barbosa <thaisbs1405@gmail.com>
2023-05-17 22:16:07 -0300

    Atualizei o código - Aula 17/05

--- calculadora-de-gorjeta/src/App.jsx
+++ calculadora-de-gorjeta/src/App.jsx
@@ -1,7 +1,13 @@
+import { Header } from './components/Header'
 import { Calculadora } from './components/Calculadora'
 
 export function App() {
   return (
+  <>
+    <Header />
+    <main>
     <Calculadora />
+    </main>
+  </>
   );
 }
---
+++ calculadora-de-gorjeta/src/components/Header.jsx
@@ -0,0 +1,7 @@
+import Logo from '../images/logo.png'
+
+export function Header() {
+    return (
+        <img src={Logo} alt="Logo do site" />
+    );
+}
